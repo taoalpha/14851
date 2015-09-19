05a2c84ca36994e8bd37f36634d5e816d33e73bb
Hongfei Li <fever324@gmail.com>
2015-09-19 11:16:17 -0400

    Add get nearby bus location function

---
+++ api/getNearbyStops.py
@@ -0,0 +1,32 @@
+import json
+import sys
+import math
+import geopy
+import geopy.distance
+    
+reload(sys)
+sys.setdefaultencoding("utf-8")
+    
+# stop - route
+    
+dataset = {}
+    
+with open("stop-locations.json","r") as fl:
+    dataset = json.load(fl)
+    
+def getNearByLocation(selfLocaiton):
+    selfLat = selfLocaiton[0]
+    selfLong = selfLocaiton[1]
+    nearByLocations = []
+    
+    pt1 = geopy.Point(selfLat, selfLong)
+    
+    for location in dataset:
+        
+        pt2 = geopy.Point(location["Latitude"], location["Longitude"])
+        dist = geopy.distance.distance(pt1, pt2).km
+        if(dist < 2):
+            nearByLocations.append(location)
+    
+    return nearByLocations
+            
